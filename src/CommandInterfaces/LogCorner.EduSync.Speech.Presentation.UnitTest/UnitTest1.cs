9996f2377fc84f65776d8fd8e9862452cf936f9a
Gora LEYE <tocane.technologies@gmail.com>
2019-02-16 15:39:46 +0100

    use a generic repository

---
+++ src/CommandInterfaces/LogCorner.EduSync.Speech.Presentation.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace LogCorner.EduSync.Speech.Presentation.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
