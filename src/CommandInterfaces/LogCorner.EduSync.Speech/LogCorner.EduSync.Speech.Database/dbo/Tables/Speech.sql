e7701e296f0e3bb1f38c0c689e9c8d8a7dbfc483
Gora LEYE <tocane.technologies@gmail.com>
2019-02-17 19:43:23 +0100

    configure dependency injection

--- src/CommandInterfaces/LogCorner.EduSync.Speech/LogCorner.EduSync.Speech.Database/dbo/Tables/Speech.sql
+++ src/CommandInterfaces/LogCorner.EduSync.Speech/LogCorner.EduSync.Speech.Database/dbo/Tables/Speech.sql
@@ -2,9 +2,10 @@
     [ID]          UNIQUEIDENTIFIER DEFAULT (newid()) NOT NULL,
     [Title]       NVARCHAR (250)   NOT NULL,
     [Description] NVARCHAR (MAX)   NOT NULL,
-    [Status]      INT              NOT NULL,
     [Url]         NVARCHAR (250)   NOT NULL,
     [Type]        INT              DEFAULT ((1)) NOT NULL,
     CONSTRAINT [PK_Presentation] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
+
+
 
--- src/CommandInterfaces/LogCorner.EduSync.Speech/LogCorner.EduSync.Speech.Presentation/Startup.cs
+++ src/CommandInterfaces/LogCorner.EduSync.Speech/LogCorner.EduSync.Speech.Presentation/Startup.cs
@@ -1,8 +1,12 @@
 ﻿using LogCorner.EduSync.Speech.Application.UseCases;
+using LogCorner.EduSync.Speech.Domain.IRepository;
+using LogCorner.EduSync.Speech.Domain.SpeechAggregate;
+using LogCorner.EduSync.Speech.Infrastructure;
 using LogCorner.EduSync.Speech.Presentation.Exceptions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -20,6 +24,18 @@ namespace LogCorner.EduSync.Speech.Presentation
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IRegisterSpeechUseCase, RegisterSpeechUseCase>();
+
+            var connectionString = Configuration["ConnectionStrings:SpeechDB"];
+
+            services.AddDbContext<DataBaseContext>(o => o.UseSqlServer(connectionString));
+
+            services.AddScoped(typeof(IRepository<,>), typeof(Repository<,>));
+
+            services.AddScoped<ISpeechRepository, SpeechRepository>();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
             services.AddScoped<IRegisterSpeechUseCase, RegisterSpeechUseCase>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
