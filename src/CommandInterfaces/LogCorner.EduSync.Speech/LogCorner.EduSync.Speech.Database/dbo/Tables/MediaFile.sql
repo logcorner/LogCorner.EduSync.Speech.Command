1f9a64899b2b0693318959f5764016dc65be9819
Gora LEYE <tocane.technologies@gmail.com>
2019-02-17 19:04:11 +0100

    create database

---
+++ src/CommandInterfaces/LogCorner.EduSync.Speech/LogCorner.EduSync.Speech.Database/dbo/Tables/MediaFile.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[MediaFile] (
+    [ID]       UNIQUEIDENTIFIER DEFAULT (newid()) NOT NULL,
+    [Url]      NVARCHAR (250)   NULL,
+    [SpeechID] UNIQUEIDENTIFIER NOT NULL,
+    CONSTRAINT [PK_MediaFile] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_MediaFile_Speech] FOREIGN KEY ([SpeechID]) REFERENCES [dbo].[Speech] ([ID])
+);
+
