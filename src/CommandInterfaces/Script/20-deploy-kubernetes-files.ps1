60d137853800c9edec817f63d265c8df6a9a9c9e
Gora LEYE <leyegora@yahoo.fr>
2019-04-20 21:24:03 +0000

    deploy kubernetes files

---
+++ src/CommandInterfaces/Script/20-deploy-kubernetes-files.ps1
@@ -0,0 +1,10 @@
+
+Write-Host "Starting switch to location of app files" -ForegroundColor Yellow
+Set-Location ~/Source/Repos/LogCorner.EduSync/src/CommandInterfaces/Kubernetes/app
+Write-Host "Successfully switching to app setup files" -ForegroundColor Green
+
+Write-Host "Starting deployment of setup files" -ForegroundColor Yellow
+kubectl apply --recursive --filename . 
+Write-Host "Successfully deployed setup files" -ForegroundColor Green
+
+$exit = read-host "Please press enter to exit"
