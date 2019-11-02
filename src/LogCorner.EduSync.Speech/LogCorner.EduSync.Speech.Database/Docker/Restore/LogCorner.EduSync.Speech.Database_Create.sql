25e236d808612409cf5c9fba7a4b157abf81db18
Gora LEYE <tocane.technologies@gmail.com>
2019-11-02 21:50:49 +0100

    update database

--- src/LogCorner.EduSync.Speech/LogCorner.EduSync.Speech.Database/Docker/Restore/LogCorner.EduSync.Speech.Database_Create.sql
+++ src/LogCorner.EduSync.Speech/LogCorner.EduSync.Speech.Database/Docker/Restore/LogCorner.EduSync.Speech.Database_Create.sql
@@ -230,19 +230,6 @@ IF fulltextserviceproperty(N'IsFulltextInstalled') = 1
 
 
 GO
-PRINT N'Creating [dbo].[MediaFile]...';
-
-
-GO
-CREATE TABLE [dbo].[MediaFile] (
-    [ID]       UNIQUEIDENTIFIER NOT NULL,
-    [Url]      NVARCHAR (250)   NULL,
-    [SpeechID] UNIQUEIDENTIFIER NOT NULL,
-    CONSTRAINT [PK_MediaFile] PRIMARY KEY CLUSTERED ([ID] ASC)
-);
-
-
-GO
 PRINT N'Creating [dbo].[Speech]...';
 
 
@@ -258,12 +245,34 @@ CREATE TABLE [dbo].[Speech] (
 
 
 GO
-PRINT N'Creating unnamed constraint on [dbo].[MediaFile]...';
+PRINT N'Creating [dbo].[EventStore]...';
 
 
 GO
-ALTER TABLE [dbo].[MediaFile]
-    ADD DEFAULT (newid()) FOR [ID];
+CREATE TABLE [dbo].[EventStore] (
+    [Id]          INT              IDENTITY (1, 1) NOT NULL,
+    [Version]     BIGINT           NOT NULL,
+    [AggregateId] UNIQUEIDENTIFIER NOT NULL,
+    [Name]        NVARCHAR (250)   NOT NULL,
+    [TypeName]    NVARCHAR (250)   NOT NULL,
+    [OccurredOn]  DATETIME         NOT NULL,
+    [PayLoad]     TEXT             NOT NULL,
+    [IsSync]      BIT              NOT NULL,
+    CONSTRAINT [PK__EventStore] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+PRINT N'Creating [dbo].[MediaFile]...';
+
+
+GO
+CREATE TABLE [dbo].[MediaFile] (
+    [ID]       UNIQUEIDENTIFIER NOT NULL,
+    [Url]      NVARCHAR (250)   NULL,
+    [SpeechID] UNIQUEIDENTIFIER NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
 
 
 GO
