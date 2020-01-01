d09576815a691912622db456ee4a2a0aef599324
Gora LEYE <tocane.technologies@gmail.com>
2020-01-01 19:32:16 +0100

    Handling Update when Expected version is not equal to aggregate version should raise ConcurrencyException

--- src/LogCorner.EduSync.Speech/LogCorner.EduSync.Speech.Application.UnitTest/UpdateSpeechUseCaseUnitTest.cs
+++ src/LogCorner.EduSync.Speech/LogCorner.EduSync.Speech.Application.UnitTest/UpdateSpeechUseCaseUnitTest.cs
@@ -6,6 +6,7 @@ using Moq;
 using System;
 using System.Threading.Tasks;
 using LogCorner.EduSync.Speech.Domain;
+using LogCorner.EduSync.Speech.Domain.Exceptions;
 using Xunit;
 
 namespace LogCorner.EduSync.Speech.Application.UnitTest
@@ -104,6 +105,39 @@ namespace LogCorner.EduSync.Speech.Application.UnitTest
             mockEventSourcingSubscriber.Verify(m=>
                 m.Subscribe(It.IsAny<IEventSourcing>()), Times.Once);
             moqUnitOfWork.Verify(m => m.Commit(), Times.Once);
+        }
+
+        [Fact(DisplayName = 
+            "Handling Update when Expected version is not equal to aggregate version should raise ConcurrencyException")]
+        public async Task HandlingUpdateWhenExpectedVersionIsNotEqualToAggregateVersionShouldRaiseConcurrencyException()
+        {
+            //Arrange
+            string title = @"Lorem Ipsum is simply dummy text";
+            string newTitle = @"New Lorem Ipsum is simply dummy text";
+            string description = @"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book";
+            string url = "http://www.test.com";
+            UpdateSpeechCommandMessage command = new UpdateSpeechCommandMessage(Guid.NewGuid(),
+                newTitle, description, url, SpeechType.Conferences.ToString(), 2);
+
+            Mock<IUnitOfWork> moqUnitOfWork = new Mock<IUnitOfWork>();
+            var mockEventSourcingSubscriber = new Mock<IEventSourcingSubscriber>();
+            Mock<ISpeechRepository> moqSpeechRepository = new Mock<ISpeechRepository>();
+
+            var speech = new Domain.SpeechAggregate.Speech(Guid.NewGuid(),
+                new Title(title), new UrlValue("http://mysite.com"),
+                new Description(description), SpeechType.Conferences);
+
+            Mock<IEventStoreRepository<Domain.SpeechAggregate.Speech>> moqEventStoreRepository =
+                new Mock<IEventStoreRepository<Domain.SpeechAggregate.Speech>>();
+            moqEventStoreRepository.Setup(m =>
+                    m.GetByIdAsync<Domain.SpeechAggregate.Speech>(It.IsAny<Guid>()))
+                .Returns(Task.FromResult(speech));
+
+            IUpdateSpeechUseCase usecase = new RegisterSpeechUseCase(moqUnitOfWork.Object, moqSpeechRepository.Object,
+                mockEventSourcingSubscriber.Object, moqEventStoreRepository.Object);
+            //Act
+            //Assert
+            await Assert.ThrowsAsync<ConcurrencyException>(() => usecase.Handle(command));
         }
     }
 }
