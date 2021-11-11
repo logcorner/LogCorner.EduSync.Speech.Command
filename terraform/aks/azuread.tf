665feedb4547bbb3b6c63e5b178038390313f988
Gora LEYE <leyegora@yahoo.fr>
2021-11-11 14:39:11 +0100

    fix

--- terraform/aks/azuread.tf
+++ terraform/aks/azuread.tf
@@ -9,8 +9,6 @@ resource "azuread_service_principal" "default" {
 
 resource "azuread_service_principal_password" "default" {
   service_principal_id = "${azuread_service_principal.default.id}"
-  # value                = "${random_string.password.result}"
-  # end_date             = "2099-01-01T01:00:00Z"
 }
 
 resource "azurerm_role_assignment" "aks_network" {
@@ -19,10 +17,16 @@ resource "azurerm_role_assignment" "aks_network" {
   principal_id         = "${azuread_service_principal.default.id}"
 }
 
-
 # add the role to the identity the kubernetes cluster was assigned
-resource "azurerm_role_assignment" "kubweb_to_acr" {
+/* resource "azurerm_role_assignment" "kubweb_to_acr" {
   scope                = "${data.azurerm_subscription.current.id}/resourceGroups/${azurerm_resource_group.default.name}/providers/Microsoft.ContainerRegistry/registries/${azurerm_container_registry.acr.name}"
   role_definition_name = "AcrPull"
   principal_id         = "${azuread_service_principal.default.id}"
+} */
+
+# add the role to the identity the kubernetes cluster was assigned
+resource "azurerm_role_assignment" "kubweb_to_acr" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.k8s.kubelet_identity[0].object_id
 }
