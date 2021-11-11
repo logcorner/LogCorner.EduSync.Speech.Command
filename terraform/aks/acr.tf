31dd4dce644897796a1a9343ec789660f6bdfc4d
Gora LEYE <leyegora@yahoo.fr>
2021-11-11 14:52:27 +0100

    disable azurerm_role_assignment

--- terraform/aks/acr.tf
+++ terraform/aks/acr.tf
@@ -3,8 +3,8 @@ locals {
 }
 resource "azurerm_container_registry" "acr" {
   name                     = local.acr_name
-  resource_group_name      = "${azurerm_resource_group.default.name}"
-  location                 = "${azurerm_resource_group.default.location}"
+  resource_group_name      = azurerm_resource_group.default.name
+  location                 = azurerm_resource_group.default.location
   sku                      = "Standard"
   admin_enabled            = true
 }
--- terraform/aks/azuread.tf
+++ terraform/aks/azuread.tf
@@ -10,23 +10,16 @@ resource "azuread_service_principal" "default" {
 resource "azuread_service_principal_password" "default" {
   service_principal_id = "${azuread_service_principal.default.id}"
 }
-
+/*
 resource "azurerm_role_assignment" "aks_network" {
   scope                = "${data.azurerm_subscription.current.id}/resourceGroups/${azurerm_resource_group.default.name}"
   role_definition_name = "Network Contributor"
   principal_id         = "${azuread_service_principal.default.id}"
 }
 
-# add the role to the identity the kubernetes cluster was assigned
-/* resource "azurerm_role_assignment" "kubweb_to_acr" {
-  scope                = "${data.azurerm_subscription.current.id}/resourceGroups/${azurerm_resource_group.default.name}/providers/Microsoft.ContainerRegistry/registries/${azurerm_container_registry.acr.name}"
-  role_definition_name = "AcrPull"
-  principal_id         = "${azuread_service_principal.default.id}"
-} */
-
-# add the role to the identity the kubernetes cluster was assigned
 resource "azurerm_role_assignment" "kubweb_to_acr" {
   scope                = azurerm_container_registry.acr.id
   role_definition_name = "AcrPull"
   principal_id         = azurerm_kubernetes_cluster.k8s.kubelet_identity[0].object_id
 }
+*/
