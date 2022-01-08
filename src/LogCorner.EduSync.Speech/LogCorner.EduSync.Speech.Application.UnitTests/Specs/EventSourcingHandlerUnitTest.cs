95e6314577c60040920665e7569571c64a0b20a5
Gora LEYE <leyegora@yahoo.fr>
2022-01-08 12:59:12 +0100

    fix build

--- src/LogCorner.EduSync.Speech/LogCorner.EduSync.Speech.Application.UnitTests/Specs/EventSourcingHandlerUnitTest.cs
+++ src/LogCorner.EduSync.Speech/LogCorner.EduSync.Speech.Application.UnitTests/Specs/EventSourcingHandlerUnitTest.cs
@@ -1,6 +1,5 @@
 using LogCorner.EduSync.Notification.Common.Hub;
 using LogCorner.EduSync.Speech.Application.Exceptions;
-using LogCorner.EduSync.Speech.Application.UseCases;
 using LogCorner.EduSync.Speech.Command.SharedKernel.Events;
 using LogCorner.EduSync.Speech.Command.SharedKernel.Serialyser;
 using LogCorner.EduSync.Speech.Domain.IRepository;
@@ -8,6 +7,7 @@ using LogCorner.EduSync.Speech.Domain.SpeechAggregate;
 using Moq;
 using System;
 using System.Threading.Tasks;
+using LogCorner.EduSync.Speech.Application.EventSourcing;
 using Xunit;
 
 namespace LogCorner.EduSync.Speech.Application.UnitTests.Specs
