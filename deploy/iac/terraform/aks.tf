00daf237b58d9871645177c07a4d79c5d69abe0f
Gora LEYE <leyegora@yahoo.fr>
2022-01-20 11:22:58 +0100

    plan

--- deploy/iac/terraform/aks.tf
+++ deploy/iac/terraform/aks.tf
@@ -3,7 +3,7 @@ resource "azurerm_kubernetes_cluster" "default" {
   location            = "${azurerm_resource_group.default.location}"
   resource_group_name = "${azurerm_resource_group.default.name}"
   dns_prefix          = "${var.dns_prefix}-${var.name}-aks-${var.environment}"
-  depends_on          = [azurerm_role_assignment.aks_network, azurerm_role_assignment.aks_acr]
+  #depends_on          = [azurerm_role_assignment.aks_network, azurerm_role_assignment.aks_acr]
 
 
   default_node_pool  {
@@ -12,13 +12,16 @@ resource "azurerm_kubernetes_cluster" "default" {
     vm_size         = "${var.node_type}"
    
   }
-
-  service_principal {
+  
+  identity {
+    type = "SystemAssigned"
+  }
+  /* service_principal {
     client_id     = "${azuread_application.default.application_id}"
     client_secret = "${azuread_service_principal_password.default.value}"
-  }
+  } */
 
-  role_based_access_control {
+ /*  role_based_access_control {
     enabled = true
-  }
+  } */
 }
--- deploy/iac/terraform/main.tf
+++ deploy/iac/terraform/main.tf
@@ -1,13 +1,39 @@
 # The Azure Active Resource Manager Terraform provider
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+     azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.0.0"
+    }
+  }
+}
 provider "azurerm" {
-  //version = "=1.36.0"
+  features {}
 }
 
 # The Azure Active Directory Terraform provider
-provider "azuread" {
-  //version = "=0.6.0"
-}
+/* terraform {
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.0.0"
+    }
+  }
+} */
 
+# Configure the Azure Active Directory Provider
+ provider "azuread" {
+  tenant_id = "f12a747a-cddf-4426-96ff-ebe055e215a3"
+}
+ 
+# Retrieve domain information
+data "azuread_domains" "example" {
+  only_initial = true
+}
 # Reference to the current subscription.  Used when creating role assignments
 data "azurerm_subscription" "current" {}
 
