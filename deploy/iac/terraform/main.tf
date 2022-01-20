3ab8a44ae94e58f5199f3c506fb54a8e27e007ae
Gora LEYE <leyegora@yahoo.fr>
2022-01-20 11:48:58 +0100

    fix authentication

--- deploy/iac/terraform/main.tf
+++ deploy/iac/terraform/main.tf
@@ -15,25 +15,14 @@ provider "azurerm" {
   features {}
 }
 
-# The Azure Active Directory Terraform provider
-/* terraform {
-  required_providers {
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "~> 2.0.0"
-    }
-  }
-} */
-
 # Configure the Azure Active Directory Provider
- provider "azuread" {
-  tenant_id = "f12a747a-cddf-4426-96ff-ebe055e215a3"
+provider "azuread" {
+  client_id     = var.client_id
+  client_secret = var.client_secret
+  tenant_id     = var.tenant_id
 }
  
-# Retrieve domain information
-data "azuread_domains" "example" {
-  only_initial = true
-}
+
 # Reference to the current subscription.  Used when creating role assignments
 data "azurerm_subscription" "current" {}
 
--- deploy/iac/terraform/variables.tf
+++ deploy/iac/terraform/variables.tf
@@ -1,3 +1,22 @@
+//Service principal
+variable "client_secret" {
+   type        = string
+  description = "Client secret of the service principal"
+  default     = "onJ7Q~Ym7vUi9viyWfiOPh4BmvQt5YqxKAMZR"
+}
+variable "client_id" {
+   type        = string
+  description = "client_id of the service principal"
+  default     = "5c4919f0-7d40-40ec-837c-8a6a73c47ed3"
+}
+
+variable "tenant_id" {
+   type        = string
+  description = "tenant_id of the service principal"
+  default     = "f12a747a-cddf-4426-96ff-ebe055e215a3"
+}
+
+
 // Naming
 variable "name" {
   type        = string
