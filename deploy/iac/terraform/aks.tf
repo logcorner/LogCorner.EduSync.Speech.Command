1cbd06a5e75c69bc50967896fca8498faee6a750
Gora LEYE <leyegora@yahoo.fr>
2022-01-27 10:45:06 +0100

    add outputs

--- deploy/iac/terraform/aks.tf
+++ deploy/iac/terraform/aks.tf
@@ -35,3 +35,13 @@ resource "azurerm_role_assignment" "aks_acr" {
   role_definition_name = "AcrPull"
   principal_id         = azurerm_kubernetes_cluster.default.kubelet_identity[0].object_id
 }
+
+output "kubernetes_cluster_name" {
+  description = "The name of the azure kubernetes service cluster"
+  value       = azurerm_kubernetes_cluster.default.name
+}
+
+output "container_registry_name" {
+  description = "The name of the azure container registry"
+  value       = azurerm_container_registry.default.name
+}
